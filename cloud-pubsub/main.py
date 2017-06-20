50364473d459fbf3dbaf296dab17bd2e03023114
Ahmet Alp Balkan <ahmetb@google.com>
2017-06-20 13:02:10 -0700

    Move to python:3-slim, address CR feedback
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- cloud-pubsub/main.py
+++ cloud-pubsub/main.py
@@ -6,9 +6,8 @@ prints to standard output.
 """
 
 import datetime
-from google.cloud import pubsub
-from google.cloud.pubsub.subscription import AutoAck
 
+from google.cloud import pubsub
 
 PUBSUB_TOPIC = 'eco'
 PUBSUB_SUBSCRIPTION = 'echo-read'
@@ -19,14 +18,13 @@ def main():
     client = pubsub.Client()
     subscription = client.topic(PUBSUB_TOPIC).subscription(PUBSUB_SUBSCRIPTION)
 
-    print 'Pulling messages from Pub/Sub subscription...'
+    print('Pulling messages from Pub/Sub subscription...')
     while True:
-        with AutoAck(subscription, max_messages=10) as ack:
+        with pubsub.subscription.AutoAck(subscription, max_messages=10) as ack:
             for _, message in list(ack.items()):
-                msg = "[{0}] ID={1} Data={2}"
-                print msg.format(datetime.datetime.now(),
-                                 message.message_id, message.data)
-
+                print("[{0}] ID={1} Data={2}".format(datetime.datetime.now(),
+                                                     message.message_id,
+                                                     message.data))
 
 if __name__ == '__main__':
     main()
