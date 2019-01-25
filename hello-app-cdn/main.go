bf2f908ed4021b6d799481259df4316af9c0b43f
Rohit Ramkumar <rramkumar@google.com>
2019-01-24 16:37:47 -0800

    Fix main.go to return header for Cache-Control

--- hello-app-cdn/main.go
+++ hello-app-cdn/main.go
@@ -48,5 +48,5 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
-	fmt.Fprintf(w, "Cache-Control: max-age=86400,public")
+	w.Header().Set("Cache-Control", "max-age=86400,public")
 }
