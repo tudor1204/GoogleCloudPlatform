1a89e9228048dd2298846e0121334193de1bcaed
Rohit Ramkumar <rramkumar@google.com>
2019-01-24 16:40:32 -0800

    Write header into response before anything else.

--- hello-app-cdn/main.go
+++ hello-app-cdn/main.go
@@ -45,8 +45,9 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
+	w.Header().Set("Cache-Control", "max-age=86400,public")
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
-	w.Header().Set("Cache-Control", "max-age=86400,public")
+	
 }
