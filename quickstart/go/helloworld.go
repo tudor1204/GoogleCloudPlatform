4de60b1876fa8b10a2c3f22f76d1befafaa7bcba
Brent Shaffer <betterbrent@google.com>
2019-06-26 11:24:53 -0700

    updates quickstarts for go review

--- quickstart/go/helloworld.go
+++ quickstart/go/helloworld.go
@@ -7,6 +7,16 @@ import (
 	"os"
 )
 
+func main() {
+	http.HandleFunc("/", handler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Listening on localhost:%s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
@@ -14,17 +24,4 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		target = "World"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", target)
-}
-
-func main() {
-	log.Print("Hello world sample started.")
-
-	http.HandleFunc("/", handler)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
