c6554b324f455370002a6bf4ba517c19057fd73f
Dina Graves Portman <dinagraves@google.com>
2020-09-22 15:38:08 -0700

    More changes

--- hello-app/main.go
+++ hello-app/main.go
@@ -45,7 +45,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
 	fmt.Fprintf(w, "Hello, Four Keys!\n")
-	fmt.Fprintf(w, "Version: 1.0.0\n")
+	fmt.Fprintf(w, "Version: 2.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
 
