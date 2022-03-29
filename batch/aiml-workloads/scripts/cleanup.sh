4e0dfb117418734a79d7c65a8a2054af3e0b93ff
Ahrar Monsur <ahrar@google.com>
2022-03-29 08:15:33 -0400

    Adds licenses to source files

--- batch/aiml-workloads/scripts/cleanup.sh
+++ batch/aiml-workloads/scripts/cleanup.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 source scripts/variables.sh
 
@@ -6,15 +19,16 @@ echo "*********************************"
 echo "Deleting GCP resources"
 echo "*********************************"
 
-# Delete the Artifact Repository repo
+echo "Deleting Artifact Registry repository..."
 gcloud artifacts repositories delete ${AR_REPO_ID} --location=${REGION}
 echo "Artifact Repository repository '${AR_REPO_ID}' deleted."
 
-# Delete the GKE cluster
+echo "Deleting GKE cluster..."
 gcloud container clusters delete ${CLUSTER_ID} \
     --project=${PROJECT_ID} --zone=${ZONE}
 echo "GKE cluster '${CLUSTER_ID}' deleted."
 
-# Delete the Filestore instance
+echo "Deleting Filestore instance..."
 gcloud filestore instances delete ${FILESTORE_ID} --zone=${ZONE}
 echo "Filestore instance '${FILESTORE_ID}' deleted."
+
--- batch/aiml-workloads/scripts/queue-jobs.sh
+++ batch/aiml-workloads/scripts/queue-jobs.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Set variables
 DATASETS_DIR="datasets"
 QUEUE_NAME="datasets"
 POD_NAME="redis-leader"
@@ -9,11 +24,15 @@ echo "Populating queue for batch training..."
 echo "**************************************"
 echo "The following datasets will be queued for processing:"
 filenames=""
+
+# Report all the files containing the training datasets
+# and create a concatenated string of filenames to add to the Redis queue
 for filepath in ${DATASETS_DIR}/training/*.pkl; do
   echo $filepath
   filenames+=" $filepath"
 done
 
+# Push filenames to a Redis queue running on the `redis-leader` GKE Pod
 QUEUE_LENGTH=$(kubectl exec ${POD_NAME} -- /bin/sh -c \
   "redis-cli rpush ${QUEUE_NAME} ${filenames}")
 
