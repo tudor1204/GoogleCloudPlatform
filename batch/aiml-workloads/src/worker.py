3c319058f8a6c7bc7ece55c946d5fd8996754bf9
Ahrar Monsur <ahrar@google.com>
2022-03-29 17:12:08 -0400

    Adds documentation to source files

--- batch/aiml-workloads/src/worker.py
+++ batch/aiml-workloads/src/worker.py
@@ -17,6 +17,7 @@ import os
 import rediswq
 from model_training import FraudDetectionModelTrainer
 
+# Initialize variables
 FILESTORE_PATH = "/mnt/fileserver/"
 TESTING_DATASET_PATH = FILESTORE_PATH + "datasets/testing/test_dataset.pkl"
 OUTPUT_DIR = FILESTORE_PATH + "output/"
@@ -24,37 +25,49 @@ REPORT_PATH = OUTPUT_DIR + "report.txt"
 CLASS_LABEL = "TX_FRAUD_SCENARIO"
 QUEUE_NAME = "datasets"
 HOST = "redis"
-# Uncomment next two lines if you do not have Kube-DNS working.
-# import os
-# HOST = os.getenv("REDIS_SERVICE_HOST")
-
-for file in os.listdir(FILESTORE_PATH):
-    print(os.path.join(FILESTORE_PATH, file))
-
 
 def main():
+    """
+    Workload which:
+      1. Claims a filename from a Redis Worker Queue
+      2. Reads the dataset from the file
+      3. Partially trains the model on the dataset
+      4. Saves a model checkpoint and generates a report on
+         the performance of the model after the partial training.
+      5. Removes the filename from the Redis Worker Queue
+      6. Repeats 1 through 5 till the Queue is empty
+    """
     q = rediswq.RedisWQ(name="datasets", host=HOST)
     print("Worker with sessionID: " + q.sessionID())
     print("Initial queue state: empty=" + str(q.empty()))
     checkpoint_path = None
     while not q.empty():
+        # Claim item in Redis Worker Queue
         item = q.lease(lease_secs=20, block=True, timeout=2)
         if item is not None:
             dataset_path = item.decode("utf-8")
             print("Processing dataset: " + dataset_path)
             training_dataset_path = FILESTORE_PATH + dataset_path
+
+            # Initialize the model training manager class
             model_trainer = FraudDetectionModelTrainer(
                 training_dataset_path,
                 TESTING_DATASET_PATH,
                 CLASS_LABEL,
                 checkpoint_path=checkpoint_path,
             )
+
+            # Train model and save checkpoint + report
             checkpoint_path = model_trainer.train_and_save(OUTPUT_DIR)
             model_trainer.generate_report(REPORT_PATH)
+
+            # Remove item from Redis Worker Queue
             q.complete(item)
         else:
             print("Waiting for work")
+
     print("Queue empty, exiting")
 
 
+# Run workload
 main()
