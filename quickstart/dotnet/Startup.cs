13ec7c14e53101bd09aa156c1e16a9441cd670ca
Olivier Bourgeois <bourgeoisor@gmail.com>
2022-10-24 13:59:35 -0400

    Fix remaining issues with dotnet quickstart

--- quickstart/dotnet/Startup.cs
+++ quickstart/dotnet/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace HelloGKE
 {
