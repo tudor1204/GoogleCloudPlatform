c864f48403e3818ccf12bdc49aca9fc5039399e8
Melissa Kendall <4390885+melissakendall@users.noreply.github.com>
2023-04-25 09:09:41 -0600

    DNS and static IP address terraform configuration

---
+++ autopilot/networking-tutorial/dns.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = "~> 1.3"
+}
+
+provider "google" {}
+
+variable "base_domin" {
+  type        = string
+  description = "Your base domain"
+}
+
+variable "name" {
+  type        = string
+  description = "Name of resources"
+  default     = "networking-tutorial"
+}
+
+data "google_client_config" "current" {}
+
+resource "google_compute_global_address" "default" {
+  name = var.name
+}
+
+resource "google_dns_managed_zone" "default" {
+  name        = var.name
+  dns_name    = "${var.name}.${var.base_domin}."
+  description = "DNS Zone for web application"
+}
+
+resource "google_dns_record_set" "a" {
+  name         = google_dns_managed_zone.default.dns_name
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.default.name
+
+  rrdatas = [google_compute_global_address.default.address]
+}
+
+resource "google_dns_record_set" "cname" {
+  name         = join(".", compact(["www", google_dns_record_set.a.name]))
+  type         = "CNAME"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.default.name
+
+  rrdatas = [google_dns_record_set.a.name]
+}
+
+output "dns_zone_name_servers" {
+  value       = google_dns_managed_zone.default.name_servers
+  description = "Write these virtual name servers in your base domain."
+}
+
+output "domain" {
+  value = trim(google_dns_record_set.a.name, ".")
+}
