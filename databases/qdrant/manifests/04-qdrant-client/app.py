4bf1df1eea1d8df9ce1484e0e997a2dabb4cfa55
aburhan <ameenah.burhan@gmail.com>
2024-03-06 15:57:24 +0000

    add region tags

--- databases/qdrant/manifests/04-qdrant-client/app.py
+++ databases/qdrant/manifests/04-qdrant-client/app.py
@@ -11,21 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# [START imports]
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 import os
 import sys
 import csv
-
+# [END imports]
 def main(query_string):
-
+# [START create_client]
     qdrant = QdrantClient(
         url="http://qdrant-database:6333", api_key=os.getenv("APIKEY"))
+# [END reate_client]
 
-    # Create a collection
+# [START create_collection]
     books = [*csv.DictReader(open('/usr/local/dataset/dataset.csv'))]
+# [END collection]
 
+# [START prepare_doc]
     documents: list[dict[str, any]] = []
     metadata: list[dict[str, any]] = []
     ids: list[int] = []
@@ -40,11 +43,15 @@ def main(query_string):
                 "publishDate": doc["publishDate"],
             }
         )
+# [END prepare_doc]
 
+# [START add_to_collection]
     # Add my_books to the collection 
     qdrant.add(collection_name="my_books", documents=documents, metadata=metadata, ids=ids, parallel=2)
+# [END add_to_collection]
 
     # Query the collection
+# [START query_collection]
     results = qdrant.query(
         collection_name="my_books",
         query_text=query_string,
@@ -54,7 +61,7 @@ def main(query_string):
         print("Title:", result.metadata["title"], "\nAuthor:", result.metadata["author"])
         print("Description:", result.metadata["document"], "Published:", result.metadata["publishDate"], "\nScore:", result.score)
         print("-----")
-
+# [END query_collection]
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         query_string = " ".join(sys.argv[1:])
