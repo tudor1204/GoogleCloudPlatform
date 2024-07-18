1e501dbdc2432f2b40e5722797ae2d8a56ccb6ca
Kiryl <kiryl_filatau@epam.com>
2024-07-18 16:15:06 +0200

    add the comments to the sh commands

--- ai-ml/secondary-boot-disk/run-commands.sh
+++ ai-ml/secondary-boot-disk/run-commands.sh
@@ -1,5 +1,7 @@
+# Ensure that the project_id is set
 gcloud config set project PROJECT_ID
 
+# Set the required environment variables
 export PROJECT_ID=$(gcloud config get project) \
 && export PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)") \
 && export REGION=REGION \
@@ -14,33 +16,42 @@ export PROJECT_ID=$(gcloud config get project) \
 && export STANDARD_CLUSTER_NAME=STANDARD_CLUSTER_NAME \
 && export STANDARD_NODEPOOL_NAME=STANDARD_NODEPOOL_NAME
 
+# Add the Hugging Face username and Hugging Face user token to the cloud secrets
 echo -n 'YOUR_HUGGINGFACE_USER_NAME' | gcloud secrets create hf-username --data-file=-
 echo -n 'YOUR_HUGGINGFACE_USER_TOKEN' | gcloud secrets create hf-token --data-file=-
 
+# create a repository in the artifact registr
 gcloud artifacts repositories create $REPOSITORY_NAME \
   --repository-format=docker \
   --location=$REGION \
   --description="repository to store the container images with the preloaded model weights and the configuration files"
 
+# Add the required permissions to the default Cloud Build service account
 gcloud projects add-iam-policy-binding projects/$PROJECT_ID \
     --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
     --role="roles/compute.instanceAdmin.v1" \
     --role="roles/iam.serviceAccountUser" \
     --condition=None
 
+# Run the Cloud Build command with substitutions
 gcloud builds submit \
   --config cloudbuild-image.yaml \
 --substitutions=_MODEL_PATH=$MODEL_PATH,_PROJECT_ID=$PROJECT_ID,_REPOSITORY_NAME=$REPOSITORY_NAME,_IMAGE_NAME=$IMAGE_NAME,_REGION=$REGION,_IMAGE_TAG=$IMAGE_TAG
 
+# Check the existing container image
 gcloud artifacts docker images list $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME
 
+# Create a Cloud Storage bucket to store the execution logs
 gsutil mb gs://$BUCKET_NAME
 
+# Run the Cloud Build command with substitutions
 gcloud builds submit --config cloudbuild-disk.yaml --no-source \
 --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$BUCKET_NAME,_ZONE=$ZONE
 
+# check the existing disk image
 gcloud compute images list --no-standard-images
 
+# Create a GKE Standard cluster
 gcloud container clusters create ${STANDARD_CLUSTER_NAME} \
   --project=${PROJECT_ID} \
   --region=${REGION} \
@@ -50,6 +61,7 @@ gcloud container clusters create ${STANDARD_CLUSTER_NAME} \
   --num-nodes=1 \
   --enable-image-streaming
 
+# Create a node pool with a secondary boot disk
 gcloud beta container node-pools create ${STANDARD_NODEPOOL_NAME} \
   --accelerator type=nvidia-l4,count=2,gpu-driver-version=latest \
   --project=${PROJECT_ID} \
@@ -62,4 +74,5 @@ gcloud beta container node-pools create ${STANDARD_NODEPOOL_NAME} \
   --enable-image-streaming \
  --secondary-boot-disk=disk-image=projects/${PROJECT_ID}/global/images/${DISK_IMAGE},mode=CONTAINER_IMAGE_CACHE
 
+# Apply the deployment and change the placeholder with the name of the container image
 sed "s|<CONTAINER_IMAGE>|$CONTAINER_IMAGE|" model-deployment.yaml | kubectl apply -f -
