cc1ce7cb4bfd1cb4a07272d1afda134d99686eb7
Taras Rudko <taras_rudko@epam.com>
2024-07-31 15:56:27 +0300

    Update deployment script

--- ai-ml/llm-multi-cluster-gw/deploy.sh
+++ ai-ml/llm-multi-cluster-gw/deploy.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 
 # Set up env variables values
-# PROJECT_ID="your-project-id"
-# REGION_1="us-central1"
-# REGION_2="europe-west4"
-# HF_TOKEN="your-HuggingFace-API-token"  
+# export HF_TOKEN=HF_TOKEN
+# export REGION_1=us-central1
+# export REGION_2=europe-west4
+# export GPU_POOL_MACHINE_TYPE_1="a2-highgpu-1g"
+# export GPU_POOL_ACCELERATOR_TYPE_1="nvidia-tesla-a100"
+# export GPU_POOL_MACHINE_TYPE_2="g2-standard-4"
+# export GPU_POOL_ACCELERATOR_TYPE_2="nvidia-l4"
+ 
 
-PROJECT_NUMBER=$(gcloud projects list \
---filter="$(gcloud config get-value project)" \
---format="value(PROJECT_NUMBER)")
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
 
 gcloud services enable container.googleapis.com \
     --project=$PROJECT_ID 
 
 # Create terraform.tfvars file 
 cat <<EOF >gke-platform/terraform.tfvars
-project_id                = "$PROJECT_ID"
-enable_autopilot          = true
-region_1                  = "$REGION_1"
-region_2                  = "$REGION_2"
-gpu_pool_machine_type     = "g2-standard-4"
-gpu_pool_accelerator_type = "nvidia-l4"
-gpu_pool_node_locations_1 = $(gcloud compute accelerator-types list --filter="zone ~ $REGION_1 AND name=nvidia-tesla-a100" --limit=2 --format=json | jq -sr 'map(.[].zone|split("/")|.[8])|tojson')
-gpu_pool_node_locations_2 = $(gcloud compute accelerator-types list --filter="zone ~ $REGION_2 AND name=nvidia-l4" --limit=2 --format=json | jq -sr 'map(.[].zone|split("/")|.[8])|tojson')
-fleet_project_id          = "$PROJECT_ID"
-enable_fleet              = true
-gateway_api_channel       = "CHANNEL_STANDARD"
+project_id                  = "$PROJECT_ID"
+enable_autopilot            = true
+region_1                    = "$REGION_1"
+region_2                    = "$REGION_2"
+gpu_pool_machine_type_1     = "$GPU_POOL_MACHINE_TYPE_1"
+gpu_pool_accelerator_type_1 = "$GPU_POOL_ACCELERATOR_TYPE_1"
+gpu_pool_machine_type_2     = "$GPU_POOL_MACHINE_TYPE_2"
+gpu_pool_accelerator_type_2 = "$GPU_POOL_ACCELERATOR_TYPE_2"
+gpu_pool_node_locations_1   = $(gcloud compute accelerator-types list --filter="zone ~ $REGION_1 AND name=$GPU_POOL_ACCELERATOR_TYPE_1" --limit=2 --format=json | jq -sr 'map(.[].zone|split("/")|.[8])|tojson')
+gpu_pool_node_locations_2   = $(gcloud compute accelerator-types list --filter="zone ~ $REGION_2 AND name=$GPU_POOL_ACCELERATOR_TYPE_2" --limit=2 --format=json | jq -sr 'map(.[].zone|split("/")|.[8])|tojson')
+fleet_project_id            = "$PROJECT_ID"
+enable_fleet                = true
+gateway_api_channel         = "CHANNEL_STANDARD"
 EOF
 
 # Create clusters
@@ -75,11 +79,8 @@ NAMESPACE=llm
 # Deploy workloads in gke-us
 kubectl config use-context gke-us
 kubectl create ns $NAMESPACE
-kubectl create secret generic hf-secret \
-  --from-literal=hf_api_token=$HF_TOKEN \
-  --dry-run=client -o yaml | kubectl apply --context=gke-us -n $NAMESPACE -f -
-
-kubectl apply -f tgi-2b-it-1.1-us.yaml -n $NAMESPACE
+kubectl apply -f secret.yaml -n $NAMESPACE
+sed -e "s/GPU_ACCELERATOR_TYPE/$GPU_POOL_ACCELERATOR_TYPE_1/g" tgi-2b-it-1.1.yaml |kubectl apply -f - -n $NAMESPACE
 # kubectl apply -f gradio-tgi-us.yaml -n $NAMESPACE
 # Deploy serviceExport 
 kubectl apply -f export-us.yaml -n $NAMESPACE
@@ -87,10 +88,8 @@ kubectl apply -f export-us.yaml -n $NAMESPACE
 # Deploy workloads in gke-eu
 kubectl config use-context gke-eu
 kubectl create ns  $NAMESPACE
-
-
 kubectl apply -f secret.yaml -n $NAMESPACE
-kubectl apply -f tgi-2b-it-1.1-eu.yaml -n $NAMESPACE
+sed -e "s/GPU_ACCELERATOR_TYPE/$GPU_POOL_ACCELERATOR_TYPE_2/g" tgi-2b-it-1.1.yaml |kubectl apply -f - -n $NAMESPACE
 # kubectl apply -f gradio-tgi-eu.yaml -n $NAMESPACE
 
 # Deploy serviceExport 
@@ -103,4 +102,3 @@ kubectl apply -f gateway.yaml -n $NAMESPACE
 sleep 360
 kubectl apply -f monitoring.yaml -n $NAMESPACE --context=gke-us
 kubectl apply -f monitoring.yaml -n $NAMESPACE --context=gke-eu
-
